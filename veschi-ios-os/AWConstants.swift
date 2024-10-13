9deaa843c1a2263eed52a083a74af823a819607c
IvanBogdantsev <bogdantsev.st@gmail.com>
2024-10-13 16:38:49 +0200

    removed some files

--- veschi-ios-os/AWConstants.swift
+++
@@ -1,7 +0,0 @@
-//
-//  AWConstants.swift
-//  veschi-ios-os
-//
-//  Created by Vanya Bogdantsev on 13.10.2024.
-//
-
--- veschi-ios-os/Keys.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Keys.swift
-//  veschi-ios
-//
-//  Created by Vanya Bogdantsev on 01.10.2024.
-//
-
-import Foundation
